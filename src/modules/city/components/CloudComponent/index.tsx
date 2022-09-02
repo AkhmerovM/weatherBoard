eb5008c450d24d0e5323de4281871ec7b4edf2b1
Akhmerov <wertfani@mail.ru>
2022-09-02 15:35:34 +0300

    fixes

--- src/modules/city/components/CloudComponent/index.tsx
+++ src/modules/city/components/CloudComponent/index.tsx
@@ -1,11 +1,13 @@
-import React  from 'react';
+import React from 'react';
 import styles from './style.less';
 
 export const CloudComponent = () => {
-    return <div className={styles.cloud}>
-        <img src="/images/clouds/cloud-01.png" alt="" className={styles.cloud1}/>
-        <img src="/images/clouds/cloud-02.png" alt="" className={styles.cloud2}/>
-        <img src="/images/clouds/cloud-03.png" alt="" className={styles.cloud3}/>
-        <img src="/images/clouds/cloud-04.png" alt="" className={styles.cloud4}/>
-    </div>;
+    return (
+        <div className={styles.cloud}>
+            <img src='/images/clouds/cloud-01.png' alt='' className={styles.cloud1} />
+            <img src='/images/clouds/cloud-02.png' alt='' className={styles.cloud2} />
+            {/* <img src="/images/clouds/cloud-03.png" alt="" className={styles.cloud3}/> */}
+            <img src='/images/clouds/cloud-04.png' alt='' className={styles.cloud4} />
+        </div>
+    );
 };
